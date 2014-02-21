28886ccc6492701b11ecbbd02c61ebaeba3e9177
Gilles Coomans <gilles.coomans@gmail.com>
2014-02-21 16:18:16 +0100

    correct unit path

--- clients/units/generic.js
+++ clients/units/generic.js
@@ -6,7 +6,7 @@
 if(typeof define !== 'function')
 	var define = require('amdefine')(module);
 
-define(["require", "deepjs/deep", "deepjs/deep-unit"], function genericAjaxTestCaseDefine(require){
+define(["require", "deepjs/deep", "deepjs/lib/unit"], function genericAjaxTestCaseDefine(require){
 
 	var unit = {
 		title:"deep-jquery-ajax/units/generic"
